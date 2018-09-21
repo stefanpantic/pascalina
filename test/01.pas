10997dbff2cac796d7aac2231ced7cdf9df8ce84
Stefan Pantic <stefanpantic13@gmail.com>
2018-09-22 00:44:31 +0200

    Added test case 01

---
+++ test/01.pas
@@ -0,0 +1,35 @@
+program asd;
+
+var
+	a, b : Integer;
+	c, d : Real;
+	e : array [1..10] of Real;
+
+procedure bar;
+begin
+	a := (b + d)/e + 123.32 + (32 - 14) / 2;
+	a := 4
+end;
+
+function foo(a, b, c : Integer) : Integer;
+begin
+	c := d;
+	d := 2
+end;
+
+begin
+	x := readln(e);
+	if color < red then
+		c := 10
+	else
+		b := 2;
+
+	i := 0;
+	while i < 10 do
+	begin
+		x := writeln(i);
+		i := i + 1
+	end;
+
+	r := 0
+end.
