7ce4af01637dc9781cb2645af51b5a9249588ad1
Stefan Pantic <stefanpantic13@gmail.com>
2018-09-22 20:13:51 +0200

    Modified parameter type passing

--- include/ast/function_prototype.hpp
+++ include/ast/function_prototype.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream> // clog, endl
 #include <vector> // vector
+#include "types.hpp"
 #include "visitor.hpp"
 
 
@@ -14,22 +15,32 @@ namespace pascalina
 	*/
 	class function_prototype
 	{
+		// Convenience
+		using id_type_pairs_impl = std::vector<std::pair<std::vector<std::string>, types::base*>>;
+		using id_type_pairs = std::vector<std::pair<std::vector<std::string>, std::unique_ptr<types::base>>>;
+
 		public:
-			explicit function_prototype(std::string id, const std::vector<std::string> &params)
-				:	m_id(std::move(id)),
-					m_params(params)
-			{ std::clog << "[[32mconstructor[0m]" << __PRETTY_FUNCTION__ << std::endl; }
+			explicit function_prototype(std::string id, id_type_pairs_impl &&params = {})
+				:	m_id(std::move(id))
+			{
+
+				for(auto &&e : params) {
+					m_params.emplace_back(e.first, std::move(e.second));
+				}
+
+				std::clog << "[[32mconstructor[0m]" << __PRETTY_FUNCTION__ << std::endl;
+			}
 
 			// Getters
 			inline const std::string &id() const { return m_id; }
-			inline const std::vector<std::string> &params() const { return m_params; }
+			inline const id_type_pairs &params() const { return m_params; }
 
 			// Accept visitor member function
 			void accept(util::visitor &v) const
 			{ v.visit(*this); }
 		private:
 			std::string m_id;
-			std::vector<std::string> m_params;
+			id_type_pairs m_params;
 	}; // class pascalina::function_prototype
 
 } // namespace pascalina
