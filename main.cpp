4ff10562281c0f17b9987f67c3b71971a9ac8daa
Stefan Pantic <stefanpantic13@gmail.com>
2018-09-24 18:46:48 +0200

    Added handling of scope variables and multiple declarations

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "parser.hpp"
 
 // Global symbol table with identifiers and types.
-extern std::unordered_map<std::string, pascalina::types::base*> type_map;
+extern std::unordered_map<std::string, std::unordered_map<std::string, std::unique_ptr<pascalina::types::base>>> symtable;
 // Program tree
 extern std::unique_ptr<pascalina::program> root;
 
@@ -15,8 +15,13 @@ main(int argc, const char **)
 	yydebug = (argc == 2);
 	yyparse();
 
-	for(auto &&e : type_map) {
-		delete e.second;
+	// Print scope variables
+	for(auto &&e1 : symtable)
+	{
+		std::cout << "Context: " << e1.first << std::endl;
+		for(auto &&e2 : e1.second) {
+			std::cout << '\t' << e2.first << std::endl;
+		}
 	}
 
 	return 0;
