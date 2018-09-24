32a177afc88ef760bab14f937db465b39614cdc7
Stefan Pantic <stefanpantic13@gmail.com>
2018-09-24 21:21:12 +0200

    Added example of semantic errors

---
+++ test/02.pas
@@ -0,0 +1,38 @@
+program tekashi;
+
+var
+	a, b : Integer;
+	c, d, a, b : Real;
+	e : array [1..10] of Real;
+
+procedure bar;
+begin
+	a := (b + d)/e + 123.32 + (32 - 14) / 2;
+	a := 4
+end;
+
+function foo(a, b, c : Integer) : Integer;
+var
+	x, y : Real;
+	b : Integer;
+begin
+	c := d;
+	d := 2
+end;
+
+begin
+	x := readln(e);
+	if color < red then
+		c := 10
+	else
+		b := 2;
+
+	i := 0;
+	while i < 10 do
+	begin
+		x := writeln(i);
+		i := i + 1
+	end;
+
+	r := 0
+end.
