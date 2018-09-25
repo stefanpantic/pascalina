71719e7540a4c2bc037da98fd2b492a25fa0a073
Stefan Pantic <sp@wingman.ai>
2018-09-25 16:03:29 +0200

    Added valid code example

---
+++ test/04.pas
@@ -0,0 +1,35 @@
+program tekashi;
+
+var
+	a, b : Integer;
+	c, d : Real;
+	e : array [1..10] of Real;
+	x, i, r : Real;
+	color, red : Integer;
+
+procedure bar;
+begin
+	a := (b + d)/e + 123.32 + (32 - 14) / 2;
+	a := 4
+end;
+
+function foo(a, b, c : Integer) : Integer;
+begin
+	c := d;
+	d := 2
+end;
+
+begin
+	if color < red then
+		c := 10
+	else
+		b := 2;
+
+	i := 0;
+	while i < 10 do
+	begin
+		i := i + 1
+	end;
+
+	r := 0
+end.
