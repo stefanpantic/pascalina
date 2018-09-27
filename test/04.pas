d7c9ecb559d1d1aa80a43548f6b59c991ebcc6cb
Stefan Pantic <stefanpantic13@gmail.com>
2018-09-27 23:26:21 +0200

    Updated examples

--- test/04.pas
+++ test/04.pas
@@ -3,16 +3,18 @@ program tekashi;
 var
 	a, b : Integer;
 	c, d : Real;
-	e : array [1..10] of Real;
 	x, i, r : Real;
 	color, red : Integer;
 
 
-function foo(a1, b1, c1 : Integer) : Integer;
+function foo(a, b, c : Integer) : Integer;
 var
 	x1 : Real;
+	e : array [1..10] of Integer;
 begin
 	x1 := 10;
+	e[4] := 100;
+	writeln(e[4]);
 	c := d + x1;
 	foo := 2
 end;
@@ -23,7 +25,9 @@ begin
 end;
 
 begin
+	a := 100;
 	i := foo(1, 1, 1);
+	writeln(a);
 	a := 1;
 	i := 10;
 	writeln(a);
---
+++ test/05.pas
@@ -0,0 +1,15 @@
+program facts;
+var
+	n, i : Integer;
+
+begin
+	n := 10;	
+	while n > 0 do
+	begin
+		writeln(n);
+		n := n - 1
+	end;
+
+	writeln(n)
+end.
+
